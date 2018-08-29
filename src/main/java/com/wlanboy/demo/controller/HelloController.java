4656bdf5cd0a9820f7327cfed5b3329c546dfce2
wlanboy <me@wlanboy.com>
2018-08-29 22:00:47 +0200

    Update for Lombok integration and simplified controller

--- src/main/java/com/wlanboy/demo/controller/HelloController.java
+++ src/main/java/com/wlanboy/demo/controller/HelloController.java
@@ -1,108 +1,59 @@
 package com.wlanboy.demo.controller;
 
-import static org.springframework.hateoas.core.DummyInvocationUtils.methodOn;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Logger;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.joda.time.DateTime;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wlanboy.demo.model.HelloParameters;
-import com.wlanboy.demo.model.Vorgang;
-import com.wlanboy.demo.repository.VorgangRepository;
+
+import static org.springframework.hateoas.core.DummyInvocationUtils.methodOn;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 @RestController
 public class HelloController {
 
-    private static final Logger logger = Logger
-            .getLogger(HelloController.class.getCanonicalName());
+	private static final Logger logger = Logger.getLogger(HelloController.class.getCanonicalName());
 
-    @Autowired
-    VorgangRepository vorgangsdatenbank;
-    
-    @RequestMapping(value = "/hello", method = RequestMethod.POST)
-    public HttpEntity<HelloParameters> hello(@RequestBody HelloParameters parameters) {
-    	
-		Vorgang vorgang = new Vorgang();
-		vorgang.setName(parameters.getTarget());
-		vorgang.setStatus(parameters.getStatus());
-		
-		vorgang = vorgangsdatenbank.save(vorgang);
-    	
-    	parameters = new HelloParameters(vorgang);
-    	parameters.add(linkTo(methodOn(HelloController.class).hellogetbyid(parameters.getIdentifier())).withSelfRel());
-        logger.info("Vorgang created.");
-        return new ResponseEntity<HelloParameters>(parameters, HttpStatus.OK);
-    }   
-    
-    @RequestMapping(value = "/hello/{identifier}", method = RequestMethod.GET)
-    public HttpEntity<HelloParameters> hellogetbyid(@PathVariable(value="identifier") Long identifier) {
-    	
-    	logger.info("ID: ("+identifier+").");
-    	
-    	Vorgang suche = vorgangsdatenbank.findById(identifier).orElse(null);
-    	if (suche != null)
-    	{
-    		logger.info("Vorgang found ("+identifier+").");
-    		
-    		HelloParameters parameters = new HelloParameters(suche);
-    		parameters.add(linkTo(methodOn(HelloController.class).hellogetbyid(parameters.getIdentifier())).withSelfRel());
-    		return new ResponseEntity<HelloParameters>(parameters, HttpStatus.OK);
-    	}
-    	else {
-    		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    	}
+	static AtomicInteger counter = new AtomicInteger(0);
 
-    }  
-    
-    @RequestMapping(value = "/hello/{identifier}", method = RequestMethod.POST)
-    public HttpEntity<HelloParameters> helloupdateid(@PathVariable(value="identifier") Long identifier, @RequestBody HelloParameters parameters) {
-    	
-    	logger.info("ID: ("+identifier+").");
-    	
-    	Vorgang suche = vorgangsdatenbank.findById(identifier).orElse(null);
-    	if (suche != null)
-    	{
-    		logger.info("Vorgang found ("+identifier+").");
-    		suche.setName(parameters.getTarget());
-    		suche.setStatus(parameters.getStatus());
-    		suche = vorgangsdatenbank.save(suche);
-    		
-    		HelloParameters updatedparameters = new HelloParameters(suche);
-    		updatedparameters.add(linkTo(methodOn(HelloController.class).hellogetbyid(updatedparameters.getIdentifier())).withSelfRel());
-    		return new ResponseEntity<HelloParameters>(updatedparameters, HttpStatus.OK);
-    	}
-    	else {
-    		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    	}
+	/**
+	 * http://127.0.0.1:8001/hello
+	 * 
+	 * @param name
+	 *            String
+	 * @return String template
+	 */
+	@RequestMapping(value = "/hello/{name}", method = RequestMethod.GET)
+	public HttpEntity<HelloParameters> hello(@PathVariable(value = "name", required = false) String name) {
+		if (name == null) {
+			name = "test";
+		}
+		HelloParameters helloString = new HelloParameters(new Long(counter.incrementAndGet()), "Hello", name);
+		helloString.add(linkTo(methodOn(HelloController.class).hello(name)).withSelfRel());
 
-    }     
-    
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public HttpEntity<List<HelloParameters>> helloall() {
-    	
-    	Iterable<Vorgang> iterable = vorgangsdatenbank.findAll();
-        List<HelloParameters> list = new ArrayList<HelloParameters>();
+		logger.info("HelloParameters created.");
+		return new ResponseEntity<HelloParameters>(helloString, HttpStatus.OK);
+	}
 
-        iterable.forEach((v)->  {
-        	HelloParameters foundparameters = new HelloParameters(v); 
-        	foundparameters.add(linkTo(methodOn(HelloController.class).hellogetbyid(foundparameters.getIdentifier())).withSelfRel()); 
-        	list.add(foundparameters);
-        });
-        
-        logger.info("Vorgang loaded ("+list.size()+").");
-        
-		return new ResponseEntity<List<HelloParameters>>(list, HttpStatus.OK);
-    }   
+	/**
+	 * http://127.0.0.1:8001/datetime
+	 * 
+	 * @return String template
+	 */
+	@RequestMapping(value = "/datetime", method = RequestMethod.GET)
+	public HttpEntity<String> datetime() {
+
+		logger.info("DateTime created.");
+		DateTime now = DateTime.now();
+		return new ResponseEntity<String>(now.toLocalDateTime().toString(), HttpStatus.OK);
+	}
 
 }
--- src/main/java/com/wlanboy/demo/model/HelloParameters.java
+++ src/main/java/com/wlanboy/demo/model/HelloParameters.java
@@ -2,53 +2,20 @@ package com.wlanboy.demo.model;
 
 import org.springframework.hateoas.ResourceSupport;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@SuppressWarnings("unused")
+@Data
+@EqualsAndHashCode(callSuper=false)
 public class HelloParameters extends ResourceSupport {
 	private Long identifier;
 	private String target;
 	private String status;
-	
-	public HelloParameters() {
-		
-	}
 
-	public HelloParameters(Long counter, String target, String status) {
-		this.identifier = counter;
-		this.target = target;
-		this.status = status;
-	}	
-	
-	public HelloParameters(Vorgang vorgang) {
-		this.identifier = vorgang.getId();
-		this.target = vorgang.getName();
-		this.status = vorgang.getStatus();
-		
-	}	
-
-	public Long getIdentifier() {
-		return identifier;
-	}
-	public void setIdentifier(Long identifier) {
+	public HelloParameters(Long identifier, String target, String status) {
 		this.identifier = identifier;
-	}
-
-	public String getTarget() {
-		return target;
-	}
-	public void setTarget(String target) {
 		this.target = target;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
 		this.status = status;
 	}
-
-	@Override
-	public String toString() {
-		return "HelloParameters [Identifier=" + identifier + ",target=" + target + ", status=" + status + "]";
-	}
-
 }
