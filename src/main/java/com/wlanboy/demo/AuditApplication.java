17665baae8365d01d9b377a4d44b882e1502f86f
wlanboy <me@wlanboy.com>
2019-10-05 21:20:08 +0200

    Added Audit application

--- src/main/java/com/wlanboy/demo/AuditApplication.java
+++ src/main/java/com/wlanboy/demo/AuditApplication.java
@@ -11,9 +11,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
-public class Application {
+public class AuditApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(AuditApplication.class, args);
     }
 
     @Bean
