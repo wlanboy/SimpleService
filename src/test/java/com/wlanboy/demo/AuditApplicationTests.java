abaa74960feedc3b72010ab2989af6307fd6b3c6
wlanboy <wlanboy@github.com>
2026-04-08 13:44:05 +0200

    org.junit.Test → org.junit.jupiter.api.Test; @TestPropertySource ergänzt

--- src/test/java/com/wlanboy/demo/AuditApplicationTests.java
+++ src/test/java/com/wlanboy/demo/AuditApplicationTests.java
@@ -1,9 +1,11 @@
 package com.wlanboy.demo;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class AuditApplicationTests {
 
 	@Test
